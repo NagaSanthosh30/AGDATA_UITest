b244a0edac2776a2b926740e1a226fccd19e5217
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-17 17:40:36 -0400

    Configuration helper methods added

---
+++ AGDATA_UITest/Utilities/ConfigurationHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGDATA_UITest.Utilities
+{
+    public static class ConfigurationHelper
+    {
+        private static IConfigurationRoot configuration;
+
+        static ConfigurationHelper()
+        {
+            // Set up the configuration to read from appsettings.json
+            var builder = new ConfigurationBuilder()
+                // Current directory path
+                .SetBasePath(Directory.GetCurrentDirectory())
+                // Add JSON file to the configuration
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            configuration = builder.Build(); // Build the configuration
+        }
+
+        // Property to get BaseUrl
+        public static string BaseUrl => configuration["BaseUrl"];
+
+        // Property to get Timeout and convert it to an integer
+        public static int Timeout => int.Parse(configuration["Timeout"]);
+    }
+}
