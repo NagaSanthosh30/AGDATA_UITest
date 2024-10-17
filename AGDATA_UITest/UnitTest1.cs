39b85b70a103cea1adfdb950ac8b7e43ddeee84a
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-17 14:24:26 -0400

    Add project files.

---
+++ AGDATA_UITest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AGDATA_UITest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
