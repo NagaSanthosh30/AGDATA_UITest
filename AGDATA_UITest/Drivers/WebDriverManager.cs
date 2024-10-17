db2d6d15dcc3ac1d187f7bc678400ce389d5e1db
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-17 14:39:09 -0400

    Added WebDriverManager class file

---
+++ AGDATA_UITest/Drivers/WebDriverManager.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGDATA_UITest.Drivers
+{
+    // To Handle WebDriver instantiation and configuration
+    public class WebDriverManager
+    {
+        public IWebDriver CreateDriver()
+        {
+            // For ChromeDriver adding implicit wait for 10seconds
+            var driver = new ChromeDriver();  
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return driver;
+        }
+    }
+
+}
